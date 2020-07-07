f1e769d6679810ab1f7d937ee3b8339ad5b805d6
Your Name <fangfangxu174@gmail.com>
2020-07-07 10:09:08 +0800

    install文件忽略

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -8,18 +8,18 @@ class Install extends CI_Controller{
         $this->load->helper('url');
 	}
 	
-	public function index(){
-	
-	  $file="install/cicms.lock";
-	   if(file_exists($file)){
-	     header("Content-type: text/html; charset=utf-8"); 
-         echo '<h1>系统检测到已经安装，若要重新安装，请删除根目录/install/cicms.lock文件再进行操作。</h1>';
-		 exit;
-       }
-	   
-		$this->load->view('install/index');
-		
-	}
+//	public function index(){
+//
+//	  $file="install/cicms.lock";
+//	   if(file_exists($file)){
+//	     header("Content-type: text/html; charset=utf-8");
+//         echo '<h1>系统检测到已经安装，若要重新安装，请删除根目录/install/cicms.lock文件再进行操作。</h1>';
+//		 exit;
+//       }
+//
+//		$this->load->view('install/index');
+//
+//	}
 
 
 }
